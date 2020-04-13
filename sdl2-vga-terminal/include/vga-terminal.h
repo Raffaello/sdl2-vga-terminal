ba0d820fcd443622d68f9889bea9e39ccf6e1485
Raffaello Bertini <raffaellobertini@gmail.com>
2020-04-13 12:02:42 +0100

    shared library demo

--- sdl2-vga-terminal/include/vga-terminal.h
+++ sdl2-vga-terminal/include/vga-terminal.h
@@ -1,13 +1,20 @@
 #pragma once
 
-//#include <vga-terminal.h>
 #include <vga-terminal-export.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-VGA_TERMINAL_EXPORT int add(int a, int b);
-//VGA_TERMINAL_EXPORT 
+	typedef void VGA_Terminal;
+	/// create a new terminal, SDL_INIT VIDEO & TIMER must be inited first.
+	VGA_TERMINAL_EXPORT VGA_Terminal* VGA_TERMINAL_init();
+	/// destroy the given terminal
+	VGA_TERMINAL_EXPORT void VGA_TERMINAL_destroy(VGA_Terminal* term);
+	/// write in the terminal
+	VGA_TERMINAL_EXPORT void VGA_TERMINAL_writeXY(VGA_Terminal* term, const uint8_t x, const uint8_t y, const char* str, const uint8_t col, const uint8_t bgCol);
+	/// render in the terminal
+	VGA_TERMINAL_EXPORT void VGA_TERMINAL_render(VGA_Terminal* term);
 
 //int add(int a, int b) { return a + b; }
 #ifdef __cplusplus
--- sdl2-vga-terminal/sdl2-vga-terminal-so.cpp
+++ sdl2-vga-terminal/sdl2-vga-terminal-so.cpp
@@ -16,18 +16,27 @@ using namespace std;
 
 int main(int argc, char* args[])
 {
-	cout << add(10, 1) << endl;
-	cin.ignore();
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+		SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
+
+
+	VGA_Terminal* term = VGA_TERMINAL_init();
+
+	VGA_TERMINAL_writeXY(term, 0, 0, "TEST", 10, 0);
+	VGA_TERMINAL_render(term);
+	SDL_Delay(1000);
+
+	VGA_TERMINAL_destroy(term);
+
 #ifdef WIN32
 	bool t = __FUnloadDelayLoadedDLL2("vga-terminal-libd.dll");
 	cout << "unloaded? :" << t << endl;
 	cin.ignore();
 #endif // WIN32
 
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-		SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Unable to initialize SDL: %s", SDL_GetError());
-		return 1;
-	}
+	
 
 	SDL_Quit();
 	return 0;
