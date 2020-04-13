5892933261c129110cb0b1b710eb9a87d9e96e14
Raffaello Bertini <raffaellobertini@gmail.com>
2020-04-13 11:13:01 +0100

    updates

--- sdl2-vga-terminal/sdl2-vga-terminal.cpp
+++ sdl2-vga-terminal/sdl2-vga-terminal.cpp
@@ -3,27 +3,11 @@
 
 #include "sdl2-vga-terminal.h"
 
-#include <vga-terminal.h>
-
-#ifdef WIN32
-#include <windows.h>
-#include <delayimp.h>
-#pragma comment(lib, "delayimp")
-
-#endif
 
 using namespace std;
 
 int main(int argc, char* args[])
 {
-	cout << add(10, 1) << endl;
-	cin.ignore();
-#ifdef WIN32
-	bool t = __FUnloadDelayLoadedDLL2("vga-terminal-libd.dll");
-	cout << "unloaded? :" << t << endl;
-	cin.ignore();
-#endif // WIN32
-	
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
