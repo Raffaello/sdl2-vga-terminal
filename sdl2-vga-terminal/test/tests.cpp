244f0c8e203b7537a31f8bc26d9127ad25101cda
Raffaello Bertini <raffaellobertini@gmail.com>
2020-04-16 12:45:14 +0100

    review codacy unused var

--- sdl2-vga-terminal/test/tests.cpp
+++ sdl2-vga-terminal/test/tests.cpp
@@ -108,7 +108,6 @@ TEST(VgaTerminal, SetViewport1)
 	std::string termTitle = "Hello Test";
 	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
 
-	auto r = term.getViewport();
 	term.setViewPort(0, 0, 1, 1);
 	auto e = term.getViewport();
 	EXPECT_EQ(0, e.x);
