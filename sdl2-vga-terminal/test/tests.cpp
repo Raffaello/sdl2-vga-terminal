fbe2e50284b434aa200f9bc16f7f0fba70832a9a
Raffaello Bertini <raffaellobertini@gmail.com>
2020-04-20 15:49:30 +0100

    parametrized unit tests

--- sdl2-vga-terminal/test/tests.cpp
+++ sdl2-vga-terminal/test/tests.cpp
@@ -113,83 +113,10 @@ TEST(VgaTerminal, SetViewportNull)
 	EXPECT_EQ(r.h, e.h);
 }
 
-class SetViewportParameterTests : public ::testing::TestWithParam<std::tuple<int, int, int, int, bool>>{};
-
-TEST_P(SetViewportParameterTests, SetVieport)
-{
-	int x = std::get<0>(GetParam());
-	int y = std::get<1>(GetParam());
-	int w = std::get<2>(GetParam());
-	int h = std::get<3>(GetParam());
-	int autoScroll = std::get<4>(GetParam());
-		
-	VgaTerminal term = VgaTerminal("SetViewport Test", SDL_WINDOW_HIDDEN, -1, 0);
-	term.autoScroll = autoScroll;
-
-	EXPECT_TRUE(term.setViewPort(x, y, w, h));
-	auto e = term.getViewport();
-	EXPECT_EQ(x, e.x);
-	EXPECT_EQ(y, e.y);
-	EXPECT_EQ(w, e.w);
-	EXPECT_EQ(h, e.h);
-	term.write("X", 15, 1);
-	auto tc = term.at(0, 0);
-	EXPECT_EQ('X', tc.c);
-	EXPECT_EQ(15, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-	DISABLED_VgaTerminal,
-	SetViewportParameterTests,
-	::testing::Values(
-		std::make_tuple(0, 0, 1, 1, false),
-		std::make_tuple(10, 10, 10, 10, true)
-	)
-);
-
-TEST(VgaTerminal, SetViewport1)
-{
-	std::string termTitle = "Hello Test";
-	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
-
-	term.setViewPort(0, 0, 1, 1);
-	auto e = term.getViewport();
-	EXPECT_EQ(0, e.x);
-	EXPECT_EQ(0, e.y);
-	EXPECT_EQ(1, e.w);
-	EXPECT_EQ(1, e.h);
-	term.write("X", 15, 0);
-	auto tc = term.at(0, 0);
-	EXPECT_EQ(0, tc.c);
-	EXPECT_EQ(0, tc.col);
-	EXPECT_EQ(0, tc.bgCol);
-}
-
-TEST(VgaTerminal, SetViewport2)
-{
-	std::string termTitle = "Hello Test";
-	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
-
-	term.setViewPort(10, 10, 10, 10);
-	auto e = term.getViewport();
-	EXPECT_EQ(10, e.x);
-	EXPECT_EQ(10, e.y);
-	EXPECT_EQ(10, e.w);
-	EXPECT_EQ(10, e.h);
-
-	term.writeXY(5, 5, "Test", 15, 1);
-	auto tc = term.at(5, 5);
-	EXPECT_EQ('T', tc.c);
-	EXPECT_EQ(15, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-}
-
 TEST(VgaTerminal, moveCursorCircle)
 {
-	std::string termTitle = "Hello Test";
-	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
-
+	VgaTerminal term = VgaTerminal("MoveCursor", SDL_WINDOW_HIDDEN, -1, 0);
+	
 	term.gotoXY(10, 10);
 	term.moveCursorLeft();
 	EXPECT_EQ(9, term.getX());
@@ -203,40 +130,6 @@ TEST(VgaTerminal, moveCursorCircle)
 	term.moveCursorUp();
 	EXPECT_EQ(10, term.getX());
 	EXPECT_EQ(10, term.getY());
-}
-
-TEST(VgaTerminal, moveCursorBorder)
-{
-	std::string termTitle = "Hello Test";
-	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
-
-	term.gotoXY(79, 23);
-	term.moveCursorRight();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(24, term.getY());
-	term.moveCursorDown();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(24, term.getY());
-	term.moveCursorLeft();
-	EXPECT_EQ(79, term.getX());
-	EXPECT_EQ(23, term.getY());
-	term.moveCursorUp();
-	EXPECT_EQ(79, term.getX());
-	EXPECT_EQ(22, term.getY());
-}
-
-TEST(VgaTerminal, ViewportMoveCursorBorder)
-{
-	std::string termTitle = "Hello Test";
-	VgaTerminal term = VgaTerminal(termTitle, SDL_WINDOW_HIDDEN, -1, 0);
-	term.setViewPort(10, 10, 10, 10);
-	term.gotoXY(9, 2);
-	term.moveCursorRight();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(3, term.getY());
-	term.moveCursorLeft();
-	EXPECT_EQ(9, term.getX());
-	EXPECT_EQ(2, term.getY());
 }
 
 TEST(VgaTerminal, NoAutoScroll)
@@ -253,125 +146,163 @@ TEST(VgaTerminal, NoAutoScroll)
 	EXPECT_EQ(21, term.getY());
 }
 
-TEST(VgaTerminal, newLine)
+class  SetViewportParameterTests : public ::testing::TestWithParam<std::tuple<int, int, int, int, bool, int, int>> {};
+TEST_P(SetViewportParameterTests, SetVieport)
 {
-	VgaTerminal term = VgaTerminal("newLine", SDL_WINDOW_HIDDEN, -1, 0);
+	int vx = std::get<0>(GetParam());
+	int vy = std::get<1>(GetParam());
+	int vw = std::get<2>(GetParam());
+	int vh = std::get<3>(GetParam());
+	bool autoScroll = std::get<4>(GetParam());
+	int x = std::get<5>(GetParam());
+	int y = std::get<6>(GetParam());
+		
+	VgaTerminal term = VgaTerminal("SetViewport Test", SDL_WINDOW_HIDDEN, -1, 0);
+	term.autoScroll = autoScroll;
 
-	term.gotoXY(10, 10);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(11, term.getY());
+	EXPECT_TRUE(term.setViewPort(vx, vy, vw, vh));
+	auto e = term.getViewport();
+	EXPECT_EQ(vx, e.x);
+	EXPECT_EQ(vy, e.y);
+	EXPECT_EQ(vw, e.w);
+	EXPECT_EQ(vh, e.h);
+	term.writeXY(x, y, "X", 15, 1);
+	auto tc = term.at(x, y);
+	EXPECT_EQ('X', tc.c);
+	EXPECT_EQ(15, tc.col);
+	EXPECT_EQ(1, tc.bgCol);
+}
+INSTANTIATE_TEST_SUITE_P(
+	VgaTerminal,
+	SetViewportParameterTests,
+	::testing::Values(
+		std::make_tuple(0, 0, 1, 1, false, 0, 0),    // SetViewport1
+		std::make_tuple(10, 10, 10, 10, true, 0, 0), // SetViewport2
+		std::make_tuple(5, 4, 20, 20, true, 5, 5)    // atViewport
+	)
+);
 
-	term.writeXY(10, 24, "test", 7, 1);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(24, term.getY());
+class  MoveCursorBorderCW : public ::testing::TestWithParam<std::tuple<int, int, int, int>> {};
+TEST_P(MoveCursorBorderCW, moveCursorBoderCW)
+{
+	SDL_Rect vp;
+	vp.x = std::get<0>(GetParam());
+	vp.y = std::get<1>(GetParam());
+	vp.w = std::get<2>(GetParam());
+	vp.h = std::get<3>(GetParam());
+	ASSERT_GT(vp.h, 3);
+	ASSERT_GT(vp.w, 2);
+
+	VgaTerminal term = VgaTerminal("SetViewport Test", SDL_WINDOW_HIDDEN, -1, 0);
+	ASSERT_TRUE(term.setViewPort(vp));
 	
+	
+	term.gotoXY(vp.w - 1, vp.h - 2);
+	term.moveCursorRight();
+	EXPECT_EQ(0, term.getX());
+	EXPECT_EQ(vp.h - 1, term.getY());
+	term.moveCursorDown();
+	EXPECT_EQ(0, term.getX());
+	EXPECT_EQ(vp.h - 1, term.getY());
+	term.moveCursorLeft();
+	EXPECT_EQ(vp.w - 1, term.getX());
+	EXPECT_EQ(vp.h - 2, term.getY());
+	term.moveCursorUp();
+	EXPECT_EQ(vp.w - 1, term.getX());
+	EXPECT_EQ(vp.h - 3, term.getY());
+}
+INSTANTIATE_TEST_SUITE_P(
+	VgaTerminal,
+	MoveCursorBorderCW,
+	::testing::Values(
+		std::make_tuple(0, 0, 80, 25),
+		std::make_tuple(10, 10, 10, 10)
+	)
+);
+
+class  NewLineParameterTests : public ::testing::TestWithParam<std::tuple<int, int, bool>> {};
+TEST_P(NewLineParameterTests, newLine)
+{
+	int x = std::get<0>(GetParam());
+	int y = std::get<1>(GetParam());
+	bool autoScroll = std::get<2>(GetParam());
+
+	VgaTerminal term = VgaTerminal("newLine", SDL_WINDOW_HIDDEN, -1, 0);
+	term.autoScroll = autoScroll;
+	auto vp = term.getViewport();
+	char c = 'X';
+	uint8_t col = 7;
+	uint8_t bgCol = 1;
+
+	ASSERT_GE(vp.h, 2);
+
+	term.gotoXY(x, y);
+	term.newLine();
+	EXPECT_EQ(0, term.getX());
+	EXPECT_EQ(x+1, term.getY());
+
+	term.writeXY(x, vp.h - 1, std::string(1, c), 7, 1);
+	term.newLine();
+	EXPECT_EQ(0, term.getX());
+	EXPECT_EQ(vp.h - 1, term.getY());
+
 	// check if it is really move on a new line, scrolling the terminal
-	auto tc = term.at(10, 23);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
+	auto tc = term.at(x, vp.h - 1 - static_cast<uint8_t>(autoScroll));
+	EXPECT_EQ(c, tc.c);
+	EXPECT_EQ(col, tc.col);
+	EXPECT_EQ(bgCol, tc.bgCol);
 }
+INSTANTIATE_TEST_SUITE_P(
+	VgaTerminal,
+	NewLineParameterTests,
+	::testing::Values(
+		std::make_tuple(10, 10, true),    // NewLine
+		std::make_tuple(10, 10, false) // no autoscroll
+	)
+);
 
-TEST(VgaTerminal, atViewport)
+class  NewLineViewPortTests : public ::testing::TestWithParam<bool> {};
+TEST_P(NewLineViewPortTests, newLineViewport)
 {
-	VgaTerminal term = VgaTerminal("atViewported", SDL_WINDOW_HIDDEN, -1, 0);
-
-	//viewport
-	term.setViewPort(5, 4, 20, 20);
-	term.writeXY(5, 5, "t", 7, 1);
-	EXPECT_EQ(6, term.getX());
-	EXPECT_EQ(5, term.getY());
-
-	auto tc = term.at(5, 5);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-}
-
-TEST(VgaTerminal, newLineViewport)
-{
+	bool autoScroll = GetParam();
 	VgaTerminal term = VgaTerminal("newLine", SDL_WINDOW_HIDDEN, -1, 0);
+	term.autoScroll = autoScroll;
+	std::string str = "X";
+	uint8_t col = 7;
+	uint8_t bgCol = 1;
 
 	//viewport
 	term.setViewPort(5, 4, 20, 20);
-	term.writeXY(5, 18, "test", 7, 1);
+	term.writeXY(5, 18, str, col, bgCol);
 	term.newLine();
 	EXPECT_EQ(0, term.getX());
 	EXPECT_EQ(19, term.getY());
 
 	auto tc = term.at(5, 18);
-	EXPECT_EQ('t', tc.c);
+	EXPECT_EQ(str[0], tc.c);
 	EXPECT_EQ(7, tc.col);
 	EXPECT_EQ(1, tc.bgCol);
 
 	term.clear();
-	term.writeXY(5, 19, "test", 7, 1);
+	term.writeXY(5, 19, str, col, bgCol);
 	term.newLine();
 	EXPECT_EQ(0, term.getX());
 	EXPECT_EQ(19, term.getY());
 
-	tc = term.at(5, 18);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
+	tc = term.at(5, 19 - static_cast<uint8_t>(autoScroll));
+	EXPECT_EQ(str[0], tc.c);
+	EXPECT_EQ(col, tc.col);
+	EXPECT_EQ(bgCol, tc.bgCol);
 }
-
-TEST(VgaTerminal, newLineNoAutoScroll)
-{
-	VgaTerminal term = VgaTerminal("newLine", SDL_WINDOW_HIDDEN, -1, 0);
-	term.autoScroll = false;
-
-	term.gotoXY(10, 10);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(11, term.getY());
-
-	term.writeXY(10, 24, "test", 7, 1);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(24, term.getY());
-
-	// check if it is NOT move on a new line, NO scrolling the terminal
-	// So basically doing just a CR on last line when autoscroll is not enabled.
-	auto tc = term.at(10, 24);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-}
-
-TEST(VgaTerminal, newLineNoAutoScrollViewport)
-{
-	VgaTerminal term = VgaTerminal("newLine", SDL_WINDOW_HIDDEN, -1, 0);
-	term.autoScroll = false;
-
-	//viewport
-	term.setViewPort(5, 4, 20, 20);
-	term.writeXY(5, 18, "test", 7, 1);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(19, term.getY());
-
-	auto tc = term.at(5, 18);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-
-	term.clear();
-	term.writeXY(5, 19, "test", 7, 1);
-	term.newLine();
-	EXPECT_EQ(0, term.getX());
-	EXPECT_EQ(19, term.getY());
-
-	tc = term.at(5, 19);
-	EXPECT_EQ('t', tc.c);
-	EXPECT_EQ(7, tc.col);
-	EXPECT_EQ(1, tc.bgCol);
-}
-
+INSTANTIATE_TEST_SUITE_P(
+	VgaTerminal,
+	NewLineViewPortTests,
+	::testing::Values(
+		true,
+		false
+	)
+);
 #endif
-
 
 
 int main(int argc, char** argv) {
