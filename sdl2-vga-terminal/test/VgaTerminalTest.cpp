90c78d7f8ab3f51d730e64ddcfbe34dc4d41168d
Raffaello Bertini <raffaellobertini@gmail.com>
2020-05-25 18:02:31 +0100

    update to v0.4.0

--- sdl2-vga-terminal/test/VgaTerminalTest.cpp
+++ sdl2-vga-terminal/test/VgaTerminalTest.cpp
@@ -36,7 +36,7 @@ void cmpTicks(const uint64_t start, const uint64_t end, const uint16_t value)
 TEST(VgaTerminal, checkVersion)
 {
     VgaTerminal t("", 0, -1, 0);
-    ASSERT_STRCASEEQ("0.3.0", t.getVersion().c_str());
+    ASSERT_STRCASEEQ("0.4.0", t.getVersion().c_str());
 }
 
 TEST(VgaTerminal, CannotInit)
