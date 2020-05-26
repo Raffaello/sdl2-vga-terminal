71399ec33a6ddc4d57a689ee1026f58a9e6ff4c8
Raffaello Bertini <raffaellobertini@gmail.com>
2020-05-26 15:52:31 +0100

    add write{,XY} tests

--- sdl2-vga-terminal/test/Environment.hpp
+++ sdl2-vga-terminal/test/Environment.hpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <SDL2/SDL.h>
+#include <string>
 
 class Environment : public ::testing::Environment
 {
@@ -14,6 +15,11 @@ public:
         ASSERT_EQ(0, SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS));
         SDL_LogSetPriority(SDL_LOG_CATEGORY_SYSTEM, SDL_LogPriority::SDL_LOG_PRIORITY_VERBOSE);
         SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LogPriority::SDL_LOG_PRIORITY_VERBOSE);
+    }
+
+    static std::string getTitle()
+    {
+        return ::testing::UnitTest::GetInstance()->current_test_info()->name();
     }
 
     static void tearDown()
